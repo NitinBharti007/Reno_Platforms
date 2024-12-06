67fd118f5e8402b219e6e85ac42fb72bbe6eeca3
Nitin Bharti <jacksp198@gmail.com>
2024-12-06 10:55:26 +0530

    Path2

--- backend/routes/schoolRoutes.js
+++ backend/routes/schoolRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./config/pool'); 
+const db = require('../config/pool'); 
 const multer = require('multer');
 const path = require('path');
 
